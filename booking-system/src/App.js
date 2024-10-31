b5201c46e6663ff396a139533345567084160b86
BEEsCODE <Abiodunprosper07@gmail.com>
2024-10-31 17:01:27 +0100

    007
    007

--- booking-system/src/App.js
+++ booking-system/src/App.js
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import LogoImage from './assets/images/LoagoL.png';
 import LogoImage2 from './assets/images/LoagoL2.png';
-import { BackgroundBeamsWithCollision } from './components/ui/background-beams-with-collision';
+// import { BackgroundBeamsWithCollisionDemo } from './components/ui/background-beams-with-collision';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
@@ -15,16 +14,14 @@ function App() {
 
   return (
     <div className={`app-container ${isLoaded ? 'open' : ''}`}>
-            <BackgroundBeamsWithCollision className="custom-background">
+      {/* <BackgroundBeamsWithCollisionDemo /> {} */}
       <div className="logo-container">
         <img src={LogoImage} className="logo" alt="Logo" />
         <img src={LogoImage2} className={`animated-logo ${isLoaded ? 'fade-in' : ''}`} alt="Animated Logo" />
       </div>
       <div className="background" />
-      </BackgroundBeamsWithCollision>
     </div>
   );
 }
-
 
 export default App;
